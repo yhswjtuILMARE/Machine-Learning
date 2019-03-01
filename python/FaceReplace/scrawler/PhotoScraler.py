2994534a2a321938123cb5cc7cc1fb0435bb2c0d
yhswjtuilmare <yh_swjtu@163.com>
2019-03-01 22:27:19 +0800

    update at March 1st

--- python/FaceReplace/scrawler/PhotoScraler.py
+++ python/FaceReplace/scrawler/PhotoScraler.py
@@ -6,10 +6,16 @@ Created By ILMARE
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 import requests
+from PIL import Image
 import os
+import re
+
+totalCount = 0
+pre_path = r"F:/tensorflow/automodel/scrawler/img/"
 
 def get_title_list(pageNum=0):
     Url = "http://tieba.baidu.com/f?kw=%E6%9D%A8%E5%B9%82&ie=utf-8&pn={0}".format(pageNum * 50)
+    print("Parsing page: ", Url)
     try:
         resp = requests.get(Url)
         bsObj = BeautifulSoup(resp.text, "html.parser")
@@ -26,18 +32,53 @@ def get_title_list(pageNum=0):
         print(e)
         return None
 
-if __name__ == "__main__":
-    Url = "http://tieba.baidu.com/p/6051406866"
+def parse_page(fronted_Url, pageNum=1):
+    Url = "{0}?pn={1}".format(fronted_Url, pageNum)
+    global totalCount
     try:
         resp = requests.get(Url)
         bsObj = BeautifulSoup(resp.text, "html.parser")
         ul = bsObj.find("ul", {"class": "l_posts_num"})
         totalPage = int(ul.find("li", {"class": "l_reply_num"}).find_all("span", {"class": "red"})[1].text)
+        print("----", "Parsing page: ", Url, ", pageNum: ", pageNum, ", totalPage: ", totalPage)
         elts = bsObj.find_all("div", {"class": ["l_post", "j_l_post", "l_post_bright", "noborder"]})
-        for elt in elts:
+        for elt, idx in zip(elts, range(len(elts))):
             div = elt.find("div", {"class": "d_post_content j_d_post_content clearfix"})
-            print(div.find_all("img"))
-        print(len(elts))
+            imgs = div.find_all("img")
+            if imgs is not None:
+                for img in imgs:
+                    src = img.attrs.get("src")
+                    res = re.match(r"^http.*/(image_emoticon)[0-9]+.(png|jpg|jpeg|gif)$", src)
+                    if res is None:
+                        ret = re.search(r"(?<=\.)(png|jpg|jpeg|gif)$", src)
+                        format = None
+                        if ret is not None:
+                            format = ret.group()
+                        if format is None:
+                            urlretrieve(src, "{0}{1}".format(pre_path, totalCount))
+                            img = Image.open("{0}{1}".format(pre_path, totalCount))
+                            format = img.format
+                            img.save("{0}{1}.{2}".format(pre_path, totalCount, format.lower()))
+                            os.remove("{0}{1}".format(pre_path, totalCount))
+                            print("-------- ", idx, ": ", src)
+                        else:
+                            urlretrieve(src, "{0}{1}.{2}".format(pre_path, totalCount, format))
+                            print("-------- ", idx, ": ", src, "format: ", format)
+                        totalCount += 1
     except Exception as e:
         print(e)
+    finally:
+        if pageNum < totalPage:
+            parse_page(fronted_Url, pageNum + 1)
+        else:
+            return
 
+if __name__ == "__main__":
+    for i in range(10):
+        return_mat = get_title_list(i * 50)
+        if return_mat is None:
+            continue
+        for (title, link, repNum), page in zip(return_mat, range(len(return_mat))):
+            if repNum <= 3000:
+                print("===>", title, ", current page: ", i + 1, ", current item: ", page)
+                parse_page(link)
